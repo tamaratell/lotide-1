f85a34f1144b88789e94934deadbe896ff9b1e05
Tamara Tell <2telltamara@gmail.com>
2023-04-04 22:14:28 -0400

    added assertAraysEqual.js to folder

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+const eqArrays = function (array1, array2) {
+  //loop over array 1 
+  console.log(array1, array2);
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return `🛑Assertion Failed: ${array1} !== ${array2}`;
+    }
+    //   if every index of array 1 matches array 2
+    //   return true
+    // else return false
+  } return `✅Assertion Passed: ${array1} === ${array2}`;
+}
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 4]));
